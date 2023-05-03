5ce66c87e4e000056541761d84ed351b5f085b6d
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-05-03 11:50:55 +0700

    tes

--- application/views/template.php
+++ application/views/template.php
@@ -238,13 +238,6 @@
 
         </div>
         <!-- /.content-wrapper -->
-        <footer class="main-footer">
-            <!-- <div class="pull-right hidden-xs">
-                <b>Version</b> 2.4.0
-            </div> -->
-            <strong>Copyright &copy; 2023 PT Tjarkindo Mas <a href="#"></a>.</strong> All rights
-            reserved.
-        </footer>
 
         <!-- Control Sidebar -->
         <aside class="control-sidebar control-sidebar-dark">
