d9b19c1ecedb033128829baf4296447851b66d13
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-05-16 14:35:03 +0700

    Tes

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -166,19 +166,105 @@ class Auth extends CI_Controller
             $input['email']         = $input['email'];
             $input['password']      = password_hash($input['password'], PASSWORD_DEFAULT);
             $input['role']          = 3;
-            $input['is_active']     = 1;
+            $input['is_active']     = 0;
             $input['foto']          = 'user.png';
             $input['agama']         = $input['agama'];
             $input['jenjang_pendidikan']     = $input['jenjang_pendidikan'];
 
+            // siapkan token
+            $token = base64_encode(random_bytes(32));
+            $user_token = [
+                'email' => $input['email'],
+                'token' => $token,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user_token', $user_token);
+
+            $this->_sendEmail($token, 'verify');
+
             $query = $this->base->insert('user', $input);
             if ($query) {
-                set_pesan('Pendaftaran berhasil.');
+                set_pesan('Pendaftaran berhasil. Silahkan cek email untuk verifikasi');
                 redirect('auth');
             } else {
                 set_pesan('gagal menyimpan ke database', false);
                 redirect('register');
             }
+        }
+    }
+
+    private function _sendEmail($token, $type)
+    {
+        $this->load->library('email');
+        $post = $this->input->post(null, TRUE);
+
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://mail.radinaltugasakhir.site',
+            'smtp_user' => 'no-reply@adinaltugasakhir.site',
+            'smtp_pass' => '1234Arema',
+            'smtp_port' => 465,
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'newline'   => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('no-reply@adinaltugasakhir.site', 'Account Verification');
+        $this->email->to($post['email']);
+
+        if ($type == 'verify') {
+            $this->email->subject('Account Verification');
+            $this->email->message('Click this link to verify you account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate</a>');
+        } else if ($type == 'forgot') {
+            $this->email->subject('Reset Password');
+            $this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+        }
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
+        }
+    }
+
+    public function verify()
+    {
+        $email = $this->input->get('email');
+        $token = $this->input->get('token');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        if ($user) {
+            $user_token = $this->db->get_where('user_token', ['token' => $token])->row_array();
+
+            if ($user_token) {
+                if (time() - $user_token['date_created'] < (60 * 60 * 24)) {
+                    $this->db->set('is_active', 1);
+                    $this->db->where('email', $email);
+                    $this->db->update('user');
+
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">' . $email . ' has been activated! Please login.</div>');
+                    redirect('auth');
+                } else {
+                    $this->db->delete('user', ['email' => $email]);
+                    $this->db->delete('user_token', ['email' => $email]);
+
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Token expired.</div>');
+                    redirect('auth');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong token.</div>');
+                redirect('auth');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">Account activation failed! Wrong email.</div>');
+            redirect('auth');
         }
     }
 }
