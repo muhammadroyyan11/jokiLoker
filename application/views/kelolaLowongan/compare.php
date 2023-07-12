e305d4abe55c1cbf84fc044eab5aa93d7f695178
Muhammad Royyan Zamzami <royyanmz87@gmail.com>
2023-07-12 12:07:27 +0700

    Testing

--- application/views/kelolaLowongan/compare.php
+++ application/views/kelolaLowongan/compare.php
@@ -14,7 +14,9 @@
                     <!-- /.tab-pane -->
                     <div class="active tab-pane" id="settings">
                         <form class="form-horizontal">
-                            <center><img class="profile-user-img img-responsive" src="<?= base_url() ?>assets/uploads/foto/<?= $satu->foto ?>" alt="User profile picture"></center><br>
+                            <center><img class="profile-user-img img-responsive" src="<?= base_url() ?>assets/uploads/foto/<?= $satu->foto ?>" alt="User profile picture"><br>
+                                <a href="<?= base_url() ?>assets/uploads/cv/<?= $satu->cv ?>" target="_blank" class="btn btn-primary"><b>Download CV</b></a>
+                            </center><br>
                             <div class="form-group">
                                 <label for="inputName" class="col-sm-2 control-label">Lowongan</label>
 
@@ -80,7 +82,10 @@
                     <!-- /.tab-pane -->
                     <div class="active tab-pane" id="settings">
                         <form class="form-horizontal">
-                        <center><img class="profile-user-img img-responsive" src="<?= base_url() ?>assets/uploads/foto/<?= $dua->foto ?>" alt="User profile picture"></center><br>
+                            <center>
+                                <img class="profile-user-img img-responsive" src="<?= base_url() ?>assets/uploads/foto/<?= $dua->foto ?>" alt="User profile picture"><br>
+                                <a href="<?= base_url() ?>assets/uploads/cv/<?= $satu->cv ?>" target="_blank" class="btn btn-primary"><b>Download CV</b></a>
+                            </center><br>
                             <div class="form-group">
                                 <label for="inputName" class="col-sm-2 control-label">Lowongan</label>
 
